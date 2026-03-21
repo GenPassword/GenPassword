d7b3506a10cfeb0600d9e2536898b23dbb1e12ad
MaksimBaydakov <wabang80@gmail.com>
2026-03-21 16:49:41 +0500

    Добавил .gitignore

--- backend/PasswordGenerator/PasswordGenerator/obj/Debug/net8.0/PasswordGenerator.AssemblyInfo.cs
+++ backend/PasswordGenerator/PasswordGenerator/obj/Debug/net8.0/PasswordGenerator.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fec99edbf07813b93f46837651e336413b41258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fc3cadf2d7641edd9dbfff77a285402b6158d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
